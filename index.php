618b2b290a6c7541458b507395eeb776eea3fcfe
GGive26 <precieudieudonne@gmail>
2023-10-11 14:12:12 -0400

    modification

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
 <body>
 <h1> Mon titre 1</h1>
 <?php
-echo"<h2>Mon petit titre</h2>";
-print_r("<h3>precieu</h3>");
-//je suis un commentaire
-$toto="otto";
-$nb=3;
-$nick="nick";
-$concat=$toto.$nick;
-echo"</br>";
-echo"</br>";
-echo"$concat";
+// echo"<h2>Mon petit titre</h2>";
+// print_r("<h3>precieu</h3>");
+// //je suis un commentaire
+// $toto="otto";
+// $nb=3;
+// $nick="nick";
+// $concat=$toto.$nick;
+// echo"</br>";
+// echo"</br>";
+// echo"$concat";
 ?>
 </body>
 </html>
